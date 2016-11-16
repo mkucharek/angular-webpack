58b519348e1f1afedada336f744188607efba0d2
Maciej Kucharek <M.Kucharek@tt.com.pl>
2016-11-16 09:26:04 +0100

    Add sample directive

--- src/app/features/home/index.js
+++ src/app/features/home/index.js
@@ -6,8 +6,9 @@ import uirouter from 'angular-ui-router';
 import routing from './home.routes';
 import HomeController from './home.controller';
 import randomNames from '../../services/randomNames.service'
+import greeting from '../../directives/greeting.directive'
 
-export default angular.module('app.home', [uirouter, randomNames])
+export default angular.module('app.home', [uirouter, randomNames, greeting])
     .config(routing)
     .controller('HomeController', HomeController)
     .name;
